8c2acb04c5296c5a88e4961bae0f500924f03c9c
Swetabh333 <swetabh333@gmail.com>
2024-11-01 11:12:15 +0530

    contest_round_171_upsolved_q1

---
+++ Educational_Codeforces_Round_171/upsolve/q1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef set<int> si;
+typedef set<char> sc;
+
+#define pb push_back
+#define F first
+#define S second
+#define dbg(x) cout << #x << " = " << x << endl
+#define fullarr(x) x.begin(), x.end()
+#define fullsort(x) sort(fullarr(x))
+#define forl(i, a, b) for (ll i = a; i < b; i++)
+#define nl "\n"
+
+const int MOD = 1000000007;
+
+void solve() {
+  int x, y, k;
+  cin >> x >> y >> k;
+  if (k <= x && k <= y) {
+    cout << 0 << " " << 0 << " " << k << " " << 0 << nl;
+    cout << 0 << " " << 0 << " " << 0 << " " << k << nl;
+    return;
+  }
+  cout << 0 << " " << 0 << " " << min(x, y) << " " << min(x, y) << nl;
+  cout << min(x, y) << " " << 0 << " " << 0 << " " << min(x, y) << nl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
