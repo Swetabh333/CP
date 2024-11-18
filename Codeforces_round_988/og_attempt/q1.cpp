0022230161408d5b386c83f8d7aa76954323a825
Swetabh333 <swetabh333@gmail.com>
2024-11-18 10:43:17 +0530

    codeforces round 988 og_attempt

---
+++ Codeforces_round_988/og_attempt/q1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef set<int> si;
+typedef set<char> sc;
+
+#define pb push_back
+#define F first
+#define S second
+#define dbg(x) cout << #x << " = " << x << endl
+#define fullarr(x) x.begin(), x.end()
+#define fullsort(x) sort(fullarr(x))
+#define forl(i, a, b) for (ll i = a; i < b; i++)
+#define nl "\n"
+
+const int MOD = 1000000007;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  vector<int> counter(n + 1, 0);
+  forl(i, 0, n) {
+    cin >> arr[i];
+    counter[arr[i]]++;
+  }
+  int score = 0;
+  forl(i, 0, n + 1) { score += counter[i] / 2; }
+  cout << score << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
